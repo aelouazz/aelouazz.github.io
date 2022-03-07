0cc37746e4f8de3d8c0809b329c524e4168e4558
aelouazz <aymane2030@gmail.com>
2022-03-07 19:16:14 +0100

    [+] fixing Home navigation

--- myscripts.js
+++ myscripts.js
@@ -1,11 +1,11 @@
-document.onkeypress = function (evt) {
-	evt = evt || window.event;
-	var charCode = evt.keyCode || evt.which;
-	var charStr = String.fromCharCode(charCode);
-	if(charStr === "q"){
-		menu();
-	}
-};
+// document.onkeypress = function (evt) {
+// 	evt = evt || window.event;
+// 	var charCode = evt.keyCode || evt.which;
+// 	var charStr = String.fromCharCode(charCode);
+// 	if(charStr === "q"){
+// 		menu();
+// 	}
+// };
 
 // THE Menu Open and Close Mechanism
 function menu(test) {
